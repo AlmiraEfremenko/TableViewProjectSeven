3bc9f879008a733722680fef72f5ed712b7be51c
AlmiraEfremenko <almira.efremenko@yandex.ru>
2021-08-12 14:20:27 +0500

    Initial Commit

---
+++ TableViewProjectSeven/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewProjectSeven
+//
+//  Created by MAC on 12.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
